1398f7854c06f804ee578bc2f7ed2204afc82e77
GCLeire <leire0612@gmail.com>
2026-05-10 20:55:14 +0200

    Modificar archivo Python con errores aun mas evidentes

--- python_file_error.py
+++ python_file_error.py
@@ -1,12 +1,41 @@
+import os
+import json
+
+config = {}
+
+def cargar_config(path="config.json", opciones={}):
+    opciones["ultima_carga"] = "ok"
+
+    try:
+        with open(path) as f:
+            data = json.load(f)
+    except:
+        pass
+
+    return data
+
 def procesar_datos(lista):
     resultado = 0
-    temp = 123
+    temp = 123  
 
     for i in range(len(lista)):
-        if lista[i] == 0:
-            return None
-        resultado += lista[i] * 2
+        if lista[i] = 0:  
+            print("cero encontrado")
+
+        resultado = resultado + lista[i]
 
     return resultado
 
-print(procesar_datos(23,45,67))
+def borrar_archivo(nombre):
+    os.system("rm -rf " + nombre)
+
+def es_valido(x):
+    return True
+    print("Esto nunca se ejecuta") 
+
+def sumar(a, b):
+    return a + b
+
+procesar_datos("hola") 
+sumar(5, "10")   
+borrar_archivo("../../etc/passwd") 
